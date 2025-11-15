e0ecc0b95f25082cab02152268d150bb05efbd8c
Lucas Olegário Alves da Silva <pekosolucas@gmail.com>
2025-11-15 16:29:08 -0300

    Add files via upload

---
+++ back-end/db.js
@@ -0,0 +1,17 @@
+// backend/db.js
+import mysql from "mysql2";
+
+export const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",       // colocar usuário MySQL
+  password: "",       // colocar senha MySQL
+  database: "KidLingo"
+});
+
+db.connect(err => {
+  if (err) {
+    console.error("Erro ao conectar ao MySQL:", err);
+    return;
+  }
+  console.log("✅ Conectado ao MySQL com sucesso!");
+});
